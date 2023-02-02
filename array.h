5d7e4515fd1be08258658944f93b8d4836845b78
mermaidariadna <arina@vanitu.ru>
2023-02-02 22:06:14 +0300

    объявление базовых функций массива

---
+++ array.h
@@ -0,0 +1,11 @@
+#ifndef _array_h_
+#define _array_h_
+#include <iostream>
+#include <time.h>
+void fillRandom(int* firstElementArr, int size);
+int arrayMax(int* arr[], int size);
+int arrayMin(int* arr[], int size);
+void ShowArray(int, int size); //
+ 
+
+#endif /*_array_h_*/
