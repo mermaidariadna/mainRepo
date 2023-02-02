cf6d779c990baeb55fcf6ef7ef4492dfaadde9b3
mermaidariadna <arina@vanitu.ru>
2023-02-02 22:07:11 +0300

    объявление функций сортировки массива

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef _sort_h_
+#define _sort_h_
+#include <iostream>
+#include "array.h"
+using namespace std;
+void bubleSort(int* , int);
+void selectionSort(int*, int);
+void countSort(int*, int);
+void insertionSort(int*, int);
+bool checkSort(int*, int);
+bool checkInsertionSort(int*, int);
+#endif /*_sort_h_*/
